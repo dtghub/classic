d6ec1d7834649f5ab09afcf9fdc34526dc638aac
Derek Todd <derekhtodd@yahoo.co.uk>
2020-01-16 22:02:21 +0000

    After bus

--- js/classic.js
+++ js/classic.js
@@ -11,7 +11,7 @@
     xobj.open('GET', 'http://localhost/json/rooms.json', true);
     xobj.onreadystatechange = function () {
       if (xobj.readyState == 4 && xobj.status == "200") {
-        //console.log(xobj.responseText);
+        console.log(xobj.responseText);
         callback(JSON.parse(xobj.responseText));
       }
     };
